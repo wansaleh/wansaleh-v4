c45908dda9575f432c9b5965550643da0b6ec37a
Wan Saleh <wansaleh@gmail.com>
2022-01-08 15:42:51 +0800

    Updates.

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,5 +1,7 @@
 import { ReactElement } from 'react';
 
+import LeavesLeft from './LeavesLeft';
+
 export default function PageTitle({
   title,
   subtitle,
@@ -9,8 +11,10 @@ export default function PageTitle({
 }) {
   return (
     <div className="mb-12">
-      <h1 className="font-semibold mx-auto relative text-5xl text-center tracking-tight lg:text-8xl">
+      <h1 className="flex font-semibold items-center justify-center mx-auto relative text-5xl text-center tracking-tight lg:text-8xl">
+        <LeavesLeft className="fill-brand h-[1em] w-[1em]" />
         <span className="relative z-10">{title}</span>
+        <LeavesLeft className="-scale-x-100 fill-brand h-[1em] transform w-[1em]" />
       </h1>
 
       {subtitle && (
