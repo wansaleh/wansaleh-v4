e9d0e57e296bb38f07bdbedc4e8ec93505705655
Wan Saleh <wansaleh@gmail.com>
2022-02-04 02:29:18 +0800

    Updates.

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -31,7 +31,7 @@ export async function getStaticProps({ params }: Params) {
       blockMap,
       posts,
     },
-    revalidate: 10,
+    revalidate: 1,
   };
 }
 
--- src/pages/code.tsx
+++ src/pages/code.tsx
@@ -16,7 +16,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
   return {
     props: { projects, tags },
-    revalidate: 10,
+    revalidate: 1,
   };
 };
 
