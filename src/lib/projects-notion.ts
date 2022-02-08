1cc225d45eddb3a13ac56e49ceffd040a966a197
Wan Saleh <wansaleh@gmail.com>
2022-02-09 00:39:27 +0800

    Updates,

--- src/lib/projects-notion.ts
+++ src/lib/projects-notion.ts
@@ -8,35 +8,21 @@ export type Project = {
   stack?: string[];
   description: string;
   defunct?: boolean;
+  hidden?: boolean;
 };
 
 export async function getAllProjects(): Promise<{
   projects: Project[];
   tags: string[];
 }> {
-  const response = await fetch(
+  const response: Project[] = await fetch(
     'https://notion-api.splitbee.io/v1/table/88bda3d3ea544970aa238a386c360054'
   ).then((res) => res.json());
 
-  const projects = response
-    .map((page: any) => {
-      return {
-        id: page.id,
-        title: page.title,
-        url: page.url,
-        thumbnail: page.thumbnailUrl,
-        tags: page.tags,
-        publishedAt: page.date,
-        stack: page.techStack,
-        description: page.description,
-        defunct: page.defunct ?? false,
-        hidden: page.hidden ?? false,
-      };
-    })
-    .sort(
-      (a, b) =>
-        new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
-    );
+  const projects = response.sort(
+    (a, b) =>
+      new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
+  );
 
   let tags: string[] = [];
 
