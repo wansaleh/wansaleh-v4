b942c58b9587b367d72019abffb112317c257490
Wan Saleh <wansaleh@gmail.com>
2022-02-11 22:47:22 +0800

    Updates.

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -30,8 +30,8 @@ export default function PageTitle({
       {subtitle && (
         <h2
           className={clsx(
-            'font-medium max-w-3xl mt-4 text-2xl text-center text-gray-500',
-            !center && '!text-left'
+            'font-medium max-w-3xl mt-4 mx-auto text-2xl text-center text-gray-500',
+            !center && '!text-left !mx-0'
           )}
         >
           {subtitle}
