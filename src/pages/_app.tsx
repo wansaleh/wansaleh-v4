edba212d2fc0769ba3e582c5b645295c9d0096de
Wan Saleh <wansaleh@gmail.com>
2022-02-20 13:00:48 +0800

    Updates.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 import { SWRConfig } from 'swr';
 
@@ -15,6 +16,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           fetch(resource, init).then((res) => res.json()),
       }}
     >
+      <Script src="https://cdn.splitbee.io/sb.js" />
+
       <ThemeProvider attribute="class">
         <Layout>
           <Component {...pageProps} />
