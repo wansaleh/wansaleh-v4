ba4cf525f0aacc5a5a0c2718d8c864f274ef7f97
Wan Saleh <wansaleh@gmail.com>
2022-04-10 11:57:37 +0800

    Update.

--- next.config.js
+++ next.config.js
@@ -33,4 +33,12 @@ module.exports = {
 
     return config;
   },
+
+  // typescript: {
+  //   // !! WARN !!
+  //   // Dangerously allow production builds to successfully complete even if
+  //   // your project has type errors.
+  //   // !! WARN !!
+  //   ignoreBuildErrors: true,
+  // },
 };
